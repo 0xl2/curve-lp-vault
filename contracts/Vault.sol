ffb52ea86c423f2c8e945581be02f6c1d6aa0f29
naruto0913 <abe.sinjiro7@gmail.com>
2023-11-08 05:15:17 +1100

    feat: contract update

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "./interfaces/IBooster.sol";
-import "./interfaces/IClaimZap.sol";
 import "./interfaces/IBaseRewardPool.sol";
 
 error ZeroAmount();
@@ -17,8 +16,8 @@ contract Vault is Ownable {
         IERC20(0xD533a949740bb3306d119CC777fa900bA034cd52);
     IERC20 public constant CVXToken =
         IERC20(0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B);
-    IClaimZap public constant CLAIMZAP =
-        IClaimZap(0x3f29cB4111CbdA8081642DA1f75B3c12DECf2516);
+    // IClaimZap public constant CLAIMZAP =
+    //     IClaimZap(0x3f29cB4111CbdA8081642DA1f75B3c12DECf2516);
     IBooster public constant CVXBooster =
         IBooster(0xF403C135812408BFbE8713b5A23a04b3D48AAE31);
     IBaseRewardPool public immutable REWARDPOOL;
@@ -42,6 +41,8 @@ contract Vault is Ownable {
     }
 
     event Deposit(address indexed account, uint256 amount);
+    event Withdraw(address indexed account, uint256 amount);
+    event Claim(address indexed account, uint256 crvReward, uint256 cvxReward);
 
     constructor(address _lpToken, uint256 _lpPid) {
         lpToken = IERC20(_lpToken);
@@ -63,7 +64,7 @@ contract Vault is Ownable {
             }
 
             if (cvxPerShare > info.cvxShare) {
-                info.cvxShare +=
+                info.cvxPending +=
                     (info.amount * (cvxPerShare - info.cvxShare)) /
                     MULTIPLIER;
                 info.cvxShare = cvxPerShare;
@@ -73,17 +74,58 @@ contract Vault is Ownable {
         if (amount != 0) info.amount += amount;
     }
 
+    function _getReward(uint256 _amount, bool _withdraw) internal {
+        if (totalDeposit == 0) return;
+
+        uint256 cvxBalance = CVXToken.balanceOf(address(this));
+        uint256 crvBalance = CRVToken.balanceOf(address(this));
+
+        if (_withdraw) {
+            REWARDPOOL.withdrawAndUnwrap(_amount, true);
+        } else {
+            REWARDPOOL.getReward();
+        }
+
+        unchecked {
+            cvxBalance = CVXToken.balanceOf(address(this)) - cvxBalance;
+            crvBalance = CRVToken.balanceOf(address(this)) - crvBalance;
+        }
+
+        if (cvxBalance != 0)
+            cvxPerShare += (cvxBalance * MULTIPLIER) / totalDeposit;
+
+        if (crvBalance != 0)
+            crvPerShare += (crvBalance * MULTIPLIER) / totalDeposit;
+    }
+
+    function _getPending(
+        UserInfo memory info
+    ) internal view returns (uint256 crvAmt, uint256 cvxAmt) {
+        crvAmt = crvPerShare > info.crvShare
+            ? info.crvPending +
+                (info.amount * (crvPerShare - info.crvShare)) /
+                MULTIPLIER
+            : info.crvPending;
+
+        cvxAmt = cvxPerShare > info.cvxShare
+            ? info.cvxPending +
+                (info.amount * (cvxPerShare - info.cvxShare)) /
+                MULTIPLIER
+            : info.cvxPending;
+    }
+
     function deposit(uint256 amount) external {
         if (amount == 0) revert ZeroAmount();
 
-        // transfer token first
-        lpToken.safeTransferFrom(msg.sender, address(this), amount);
-
-        // deposit to convex
-        CVXBooster.deposit(lpPID, amount, true);
+        // get reward first
+        _getReward(0, false);
 
         // update user info
         _updateUserInfo(amount);
+
+        // transfer token then deposit
+        lpToken.safeTransferFrom(msg.sender, address(this), amount);
+        CVXBooster.deposit(lpPID, amount, true);
 
         // update totalDeposit
         totalDeposit += amount;
@@ -97,6 +139,53 @@ contract Vault is Ownable {
         UserInfo storage info = userInfo[msg.sender];
         if (amount > info.amount) amount = info.amount;
 
-        //
+        // get reward first
+        _getReward(amount, true);
+
+        // update user info
+        (uint256 crvAmt, uint256 cvxAmt) = _getPending(info);
+        unchecked {
+            // if withdraw all them remove user info
+            if (amount == info.amount) delete userInfo[msg.sender];
+            else {
+                info.amount -= amount;
+                info.crvShare = crvPerShare;
+                info.cvxShare = cvxPerShare;
+                info.crvPending = 0;
+                info.cvxPending = 0;
+            }
+        }
+
+        // transfer tokens to user
+        if (crvAmt != 0) CRVToken.safeTransfer(msg.sender, crvAmt);
+        if (cvxAmt != 0) CVXToken.safeTransfer(msg.sender, cvxAmt);
+        lpToken.safeTransfer(msg.sender, amount);
+
+        emit Withdraw(msg.sender, amount);
     }
+
+    function claim() external {
+        // get reward first
+        _getReward(0, false);
+
+        // update user info
+        UserInfo storage info = userInfo[msg.sender];
+        (uint256 crvAmt, uint256 cvxAmt) = _getPending(info);
+        unchecked {
+            info.crvShare = crvPerShare;
+            info.cvxShare = cvxPerShare;
+            info.crvPending = 0;
+            info.cvxPending = 0;
+        }
+
+        // transfer tokens to user
+        if (crvAmt != 0) CRVToken.safeTransfer(msg.sender, crvAmt);
+        if (cvxAmt != 0) CVXToken.safeTransfer(msg.sender, cvxAmt);
+
+        emit Claim(msg.sender, crvAmt, cvxAmt);
+    }
+
+    function pendingReward(
+        address account
+    ) external view returns (uint256 crvReward, uint256 cvxReward) {}
 }
--- contracts/interfaces/IBaseRewardPool.sol
+++ contracts/interfaces/IBaseRewardPool.sol
@@ -4,8 +4,5 @@ pragma solidity ^0.8.4;
 interface IBaseRewardPool {
     function withdrawAndUnwrap(uint256 amount, bool claim) external;
 
-    function getReward(
-        address _account,
-        bool _claimExtras
-    ) external returns (bool);
+    function getReward() external returns (bool);
 }
